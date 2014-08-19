f7cee64a0f78397d06df8732a2f804ee49f625c1
Ivan Silva Neto <ivanneto@gmail.com>
2014-08-19 14:12:02 -0300

    Prosseguindo no livro...

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,22 @@
 module.exports = function (grunt) {
   grunt.initConfig({
+    jshint: {
+      all: ['src/**/*.js', 'test/**/*.js'],
+      options: {
+        globals: {
+          _: false,
+          $: false,
+          jasmine: false,
+          describe: false,
+          it: false,
+          expect: false,
+          beforeEach: false
+        },
+        browser: true,
+        devel: true
+      }
+    }
   });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 };
