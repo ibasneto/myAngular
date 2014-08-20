5946bbcc8dae33468a449286202fed7c90589448
Ivan Silva Neto <ivanneto@gmail.com>
2014-08-20 08:18:01 -0300

    Configurando o testem.

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,4 +31,6 @@ module.exports = function (grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-testem');
+
+  grunt.registerTask('default', ['testem:run:unit']);
 };
