8493805ecfa9f593d7e28201f77e6c49de95f68c
Ivan Silva Neto <ivanneto@gmail.com>
2014-08-23 15:35:46 -0300

    Página 16 do livro.

--- src/scope.js
+++ src/scope.js
@@ -4,15 +4,34 @@
 function Scope() {
   this.$$watchers = [];
 }
+function initWatchVal() {}
 Scope.prototype.$watch = function (watchFn, listenerFn) {
   var watcher = {
     watchFn: watchFn,
-    listenerFn: listenerFn
+    listenerFn: listenerFn || function () {},
+    last: initWatchVal
   };
   this.$$watchers.push(watcher);
 };
 Scope.prototype.$digest = function () {
+  var dirty;
+  do {
+    dirty = this.$$digestOnce();
+  } while(dirty);
+};
+Scope.prototype.$$digestOnce = function () {
+  var self = this, dirty = false;
   _.forEach(this.$$watchers, function (watcher) {
-    watcher.listenerFn();
+    var newValue = watcher.watchFn(self);
+    var oldValue = watcher.last;
+    if (newValue !== oldValue) {
+      watcher.last = newValue;
+      if (oldValue === initWatchVal) {
+        oldValue = newValue;
+      }
+      watcher.listenerFn(newValue, oldValue, self);
+      dirty = true;
+    }
   });
+  return dirty;
 };
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -23,5 +23,82 @@ describe('Scope', function () {
       scope.$digest();
       expect(listenerFn).toHaveBeenCalled();
     });
+
+    it('calls the watch function with the scope as the argument', function () {
+      var watchFn = jasmine.createSpy();
+      var listenerFn = function () {};
+      scope.$watch(watchFn, listenerFn);
+      scope.$digest();
+      expect(watchFn).toHaveBeenCalledWith(scope);
+    });
+
+    it('calls the listener function when the watched value changes', function () {
+      scope.someValue = 'a';
+      scope.counter = 0;
+      scope.$watch(
+        function (scope) { return scope.someValue; },
+        function (newValue, oldValue, scope) { scope.counter++; }
+      );
+
+      expect(scope.counter).toBe(0);
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+      scope.someValue = 'b';
+      expect(scope.counter).toBe(1);
+      scope.$digest();
+      expect(scope.counter).toBe(2);
+    });
+
+    it('calls listener when watch value is first undefined', function () {
+      scope.counter = 0;
+      scope.$watch(
+        function (scope) { return scope.someValue; },
+        function (newValue, oldValue, scope) { scope.counter++; }
+      );
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+    });
+
+    it('calls listener with the new value as old value the first time', function () {
+      var oldValueGiven;
+      scope.someValue = 123;
+      scope.$watch(
+        function (scope) { return scope.someValue; },
+        function (newValue, oldValue, scope) { oldValueGiven = oldValue; }
+      );
+      scope.$digest();
+      expect(oldValueGiven).toBe(scope.someValue);
+    });
+
+    it('may have watchers that omit the listener function', function () {
+      var watchFn = jasmine.createSpy().and.returnValue('something');
+      scope.$watch(watchFn);
+      scope.$digest();
+      expect(watchFn).toHaveBeenCalled();
+    });
+
+    it('triggers chained watchers in the same digest', function () {
+      scope.name = 'Jane';
+      scope.$watch(
+        function (scope) { return scope.nameUpper; },
+        function (newValue, oldValue, scope) {
+          if (newValue) {
+            scope.initial = newValue.substring(0, 1) + '.';
+          }
+        }
+      );
+      scope.$watch(
+        function (scope) { return scope.name; },
+        function (newValue, oldValue, scope) {
+          if (newValue) {
+            scope.nameUpper = newValue.toUpperCase();
+          }
+        }
+      );
+      scope.$digest();
+      expect(scope.initial).toBe('J.');
+    });
   });
 });
